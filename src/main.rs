7e2f930d144b85b1595cb10da0b2e67c49356ef5
Balasubramanian Palaniappan <bpalaniappan@jobcase.com>
2024-04-24 21:18:16 -0500

    add user to DB working fine

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use axum::extract::State;
-use axum::http::{HeaderMap, StatusCode};
-use axum::response::{IntoResponse, Response};
-use axum::routing::get;
+use std::env;
+use std::net::SocketAddr;
+
 use axum::{Json, Router};
+use axum::extract::State;
+use axum::http::{header, HeaderMap, StatusCode};
+use axum::response::{IntoResponse, Response};
+use axum::routing::{get, post};
 use serde::{Deserialize, Serialize};
-use sqlx::postgres::PgPoolOptions;
 use sqlx::{Error, PgPool};
+use sqlx::postgres::PgPoolOptions;
 use tracing::{error, info, warn};
 
 #[tokio::main]
@@ -15,6 +18,9 @@ async fn main() {
 
     dotenvy::dotenv().ok();
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let server_host = env::var("SERVER_HOST").expect("Error getting server host");
+    let server_port = env::var("SERVER_PORT").expect("Error getting server port");
+    let server_addr = server_host + ":" + &*server_port;
 
     // Setup connection pool.
     let pool = PgPoolOptions::new()
@@ -29,10 +35,15 @@ async fn main() {
         .unwrap();
 
     // build our application with a route
-    let app = Router::new().route("/", get(handler_json)).with_state(pool);
+    let app = Router::new()
+        .route("/", get(handler_json))
+        .route("/users", post(handler_create_user))
+        .with_state(pool);
 
     // run it
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let server_address: SocketAddr = server_addr.parse().unwrap();
+    info!("Starting server at {}", server_addr);
+    let listener = tokio::net::TcpListener::bind(server_address).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
@@ -82,4 +93,51 @@ async fn handler_json(State(pool): State<PgPool>, headers: HeaderMap) -> Respons
         }),
     )
         .into_response()
+}
+
+async fn handler_create_user(
+    State(pool): State<PgPool>,
+    Json(user_request): Json<UserRequest>,
+) -> Response {
+    create_user(pool, user_request).await.into_response()
+}
+
+async fn create_user(pool: PgPool, user_request: UserRequest) -> Response {
+    let result = sqlx::query!(
+        "INSERT INTO \"user\" (first_name, last_name, email) VALUES ($1, $2, $3) RETURNING id",
+        &user_request.first_name,
+        &user_request.last_name,
+        &user_request.email
+    )
+    .fetch_one(&pool)
+    .await;
+
+    match result {
+        Ok(user) => (
+            StatusCode::CREATED,
+            [(header::LOCATION, format!("/users/{}", user.id))],
+            Json(Message {
+                message: "Success".to_string(),
+                status: format!("User created with ID: {}", user.id),
+            }),
+        )
+            .into_response(),
+        Err(_) => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(Message {
+                message: "Error".to_string(),
+                status: "Error creating user".to_string(),
+            }),
+        )
+            .into_response(),
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct UserRequest {
+    #[serde(rename = "firstName")]
+    first_name: String,
+    #[serde(rename = "lastName")]
+    last_name: String,
+    email: String,
 }
